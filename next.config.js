79bc887a33507796a21eb4364c632c72c50a569f
sammy171207 <sameerrandive171207@gmail.com>
2025-08-29 20:53:35 +0530

    change

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   experimental: {},
   images: {
-    domains: ['res.cloudinary.com', 'firebasestorage.googleapis.com', 'img.icons8.com', 'raw.githubusercontent.com', 'i.imgur.com', 'img.freepik.com','media.geeksforgeeks.org','appwrite.io','i.postimg.cc']
+    domains: ['res.cloudinary.com', 'firebasestorage.googleapis.com', 'img.icons8.com', 'raw.githubusercontent.com', 'i.imgur.com', 'img.freepik.com','media.geeksforgeeks.org','appwrite.io','i.postimg.cc','drive.google.com']
   }
 }
 
