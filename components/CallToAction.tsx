7fd5d31d01596dd28606383ca2881328aff28d4d
sammy171207 <sameerrandive171207@gmail.com>
2025-09-01 14:10:31 +0530

    update copyright year in LICENSE and change GitHub link in CallToAction component

--- components/CallToAction.tsx
+++ components/CallToAction.tsx
@@ -7,7 +7,7 @@ const CallToAction = () => {
             <div className="xl:max-w-6xl mx-auto md:mx-6 lg:mx-10 xl:mx-auto py-16 md:py-24 text-center text-white">
                 <h2 className="text-3xl md:text-4xl font-bold">See the source code</h2>
                 <p className="text-lg mt-4">This portfolio is open source and you can see the source code on GitHub.</p>
-                <Link href={'https://github.com/saviomartin/nextjs-portfolio'} target="_blank" className="mt-8 bg-white text-violet-600 px-6 py-3 rounded-md font-semibold text-lg inline-flex items-center gap-2"> <FaGithub /> See on GitHub</Link>
+                <Link href={'https://github.com/sammy171207'} target="_blank" className="mt-8 bg-white text-violet-600 px-6 py-3 rounded-md font-semibold text-lg inline-flex items-center gap-2"> <FaGithub /> See on GitHub</Link>
             </div>
         </div>
     )
